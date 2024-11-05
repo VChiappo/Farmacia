aa5df398ca42cf9d66f64ea8ce7f33e4d539143d
Victoria <victoriachiappo@gmail.com>
2024-11-05 08:59:14 -0300

    Agg cards para proveedores y funcion editar logo

--- js/Proveedor.js
+++ js/Proveedor.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var funcion;
     buscar_prov();
 
@@ -8,41 +8,111 @@ $(document).ready(function(){
         let telefono = $('#telefono').val();
         let correo = $('#correo').val();
         let direccion = $('#direccion').val();
-        funcion ='crear';
-        $.post('../controlador/ProveedorController.php',{funcion, nombre, telefono, correo, direccion },(response) =>{
-           if(response=="add"){
-            $('#add-prov').hide('slow');
-            $('#add-prov').show(1000);
-            $('#add-prov').hide(2000);
-            $('#form-crear').trigger('reset');
+        funcion = 'crear';
+        $.post('../controlador/ProveedorController.php', { funcion, nombre, telefono, correo, direccion }, (response) => {
+            if (response == "add") {
+                $('#add-prov').hide('slow');
+                $('#add-prov').show(1000);
+                $('#add-prov').hide(2000);
+                $('#form-crear').trigger('reset');
 
-           }
-           if(response=="noadd"){
-            $('#noadd-prov').hide('slow');
-            $('#noadd-prov').show(1000);
-            $('#noadd-prov').hide(2000);
-            $('#form-crear').trigger('reset');        
+            }
+            if (response == "noadd") {
+                $('#noadd-prov').hide('slow');
+                $('#noadd-prov').show(1000);
+                $('#noadd-prov').hide(2000);
+                $('#form-crear').trigger('reset');
 
 
-           }
+            }
         });
     });
     function buscar_prov(consulta) {
         funcion = 'buscar';
         $.post('../controlador/ProveedorController.php', { consulta, funcion }, (response) => {
-            console.log(response);
-    });
-}
+            const proveedores = JSON.parse(response);
+            let template='';
+            proveedores.forEach(proveedor => {
+                template+= `
+                <div provId="${proveedor.id}"provNombre="${proveedor.nombre}" provTelefono="${proveedor.telefono}"provCorreo="${proveedor.correo}" provDireccion="${proveedor.direccion}" provAvatar="${proveedor.avatar}"class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+              <div class="card bg-light">
+                <div class="card-header text-muted border-bottom-0"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+                 <h1 class='badge badge-success'>Proveedor</h1>
+                </font></font></div>
+                <div class="card-body pt-0">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">"${proveedor.nombre}"</font></font></b></h2>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Dirección: "${proveedor.direccion}"</font></font></li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Teléfono: "${proveedor.telefono}"</font></font></li>
+                         <li class="small"><span class="fa-li"><i class="fas fa-lg fa-at"></i></span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Correo: "${proveedor.correo}"</font></font></li>
+    
+                        </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="${proveedor.avatar}" alt="" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+                    <button class="avatar btn btn-sm btn-info" title="Editar logo" type="button" data-toggle="modal" data-target="#cambiologo">
+                      <i class="fas fa-image"></i>
+                    </button>
+                     <button class="avatar btn btn-sm btn-success" title="Editar proveedor">
+                      <i class="fas fa-pencil-alt"></i>
+                    </button>
+                     <button class="avatar btn btn-sm btn-danger" title="Eliminar proveedor">
+                      <i class="fas fa-trash-alt"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+                `;
+            });
+            $('#proveedores').html(template);
+        });
+    }
 
-$(document).on('keyup','#buscar_proveedor',function(){
-    let valor = $(this).val();
-        if (valor != ''){
+    $(document).on('keyup', '#buscar_proveedor', function () {
+        let valor = $(this).val();
+        if (valor != '') {
             buscar_prov(valor);
         }
         else {
             buscar_prov();
         }
 
-});
+    });
 
+   /* $(document).on('click', '.avatar', (e)=>{
+        funcion="cambiar_logo";
+        const elemento =$(this)[0].activeElement.parentElement.parentElement.parentElement.parentElement;
+        const  id = $(elemento).attr('provId');
+        console.log(id);
+        /*const nombre = $(elemento).attr('provNombre');
+        const avatar = $(elemento).attr('provAvatar');
+        $('#logoactual').attr('src', avatar);
+        $('#nombre_logo').html(nombre);
+        $('#id_logo_prov').val(id);
+        $('#funcion').val(funcion);
+    });*/
+
+    $(document).on('click', '.avatar', (e) => {
+        funcion = "cambiar_logo";
+        const elemento = $(e.currentTarget).closest('.col-12'); // Seleccionamos el contenedor del proveedor
+        const id = $(elemento).attr('provId');
+        const nombre = $(elemento).attr('provNombre');
+        const avatar = $(elemento).attr('provAvatar');
+        
+        console.log(id, nombre, avatar); // para verificar que los valores sean correctos
+        
+        $('#logoactual').attr('src', avatar);
+        $('#nombre_logo').html(nombre);
+        $('#id_logo_prov').val(id);
+        $('#funcion').val(funcion);
+    });
+    
 });
--- vista/adm_proveedor.php
+++ vista/adm_proveedor.php
@@ -9,7 +9,43 @@ if ($_SESSION['us_tipo'] == 1 || $_SESSION['us_tipo'] == 3) {
   <?php
   include_once "layouts/nav.php";
   ?>
-
+  <div class="modal fade" id="cambiologo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Cambiar logo</h1>
+          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="text-center">
+            <img id="logoactual" src="../img/avatar.png" class="profile-user-img img-fluid img-circle">
+          </div>
+          <div class="text-center">
+            <b id="nombre_logo"></b>
+          </div>
+          <div class="alert alert-success text-center" id="edit-prov" style="display: none;">
+            <span><i class="fas fa-check m-1"></i>El logo se edito</span>
+          </div>
+          <div class="alert alert-danger text-center" id="noedit-prov" style="display: none;">
+            <span><i class="fas fa-times m-1"></i>Formato no soportado</span>
+          </div>
+          <form id="form-logo" enctype="multipart/form-data">
+            <div class="input-group mb-3 ml-5 mt-2">
+              <input type="file" name="photo" class="input-group">
+              <input type="hidden" name="funcion" id="funcion">
+              <input type="hidden" name="id_logo_prov" id="id_logo_prov">
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Cerrar</button>
+          <button type="submit" class="btn bg-gradient-primary">Guardar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="modal fade" id="crearproveedor" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
