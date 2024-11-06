ee29a6b27f29ec51704304975ff2ff020bf8923b
AntonellaRe <ma.antonellare@gmail.com>
2024-11-05 21:43:09 -0300

    Alertas para lotes vencidos o por vencerse

--- js/Lote.js
+++ js/Lote.js
@@ -9,9 +9,17 @@ $(document).ready(function () {
             let template = '';
             lotes.forEach(lote => {
                 template += `
-                <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
-                <div class="card bg-light">
-                    <div class="card-header text-muted border-bottom-0"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+                <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">`;
+                if(lote.estado=='light'){
+                    template += `<div class="card bg-light">`;
+                }
+                if(lote.estado=='danger'){
+                    template += `<div class="card bg-danger">`;
+                }
+                if(lote.estado=='warning'){
+                    template += `<div class="card bg-warning">`;
+                }
+                    template += `<div class="card-header border-bottom-0"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
                         <i class="fas fa-lg fa-cubes mr-1"></i>${lote.stock}
                         </font></font>
                     </div>
@@ -20,7 +28,7 @@ $(document).ready(function () {
                         <div class="col-7">
                         <h2 class="lead"><b>${lote.nombre}</b></h2>
                        
-                        <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <ul class="ml-4 mb-0 fa-ul">
                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-mortar-pestle"></i></span> Concentracion: ${lote.concentracion}</li>
                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-prescription-bottle-alt"></i></span> Adicional: ${lote.adicional}</li>
                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-flask"></i></span> Laboratorio: ${lote.laboratorio}</li>
@@ -28,6 +36,8 @@ $(document).ready(function () {
                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-pills"></i></span> Presentación: ${lote.presentacion}</li>
                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-calendar-times"></i></span> Vencimiento: ${lote.vencimiento}</li>
                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-truck"></i></span> Proveedor: ${lote.proveedor}</li>
+                            <li class="small"><span class="fa-li"><i class="fas fa-lg fa-calendar-alt"></i></span> Mes: ${lote.mes}</li>
+                            <li class="small"><span class="fa-li"><i class="fas fa-lg fa-calendar-day"></i></span> Día: ${lote.dia}</li>
                         </ul>
                         </div>
                         <div class="col-5 text-center">
@@ -37,16 +47,11 @@ $(document).ready(function () {
                     </div>
                     <div class="card-footer">
                     <div class="text-right">
-                        <button class="avatar btn btn-sm bg-teal" type= "button" data-toggle="modal" data-target="#cambiologo">
-                        <i class="fas fa-image"></i>
-                        </button>
-                        <button class="editar btn btn-sm btn-success" type= "button" data-toggle="modal" data-target="#crearlote" >
                        
+                        <button class="editar btn btn-sm btn-success" type= "button" data-toggle="modal" data-target="#crearlote" >
                         <i class="fas fa-pencil-alt"></i>
                         </button>
-                        <button class="lote btn btn-sm btn-primary" type= "button" data-toggle="modal" data-target="#crearlote">
-                        <i class="fas fa-plus-square"></i>
-                        </button>
+                        
                         <button class="borrar btn btn-sm btn-danger">
                         <i class="fas fa-trash-alt"></i>
                         </button>
